51d4f6a375a8a4636f7b963005122f060ec60f48
Ian White <ian.w.white@gmail.com>
2008-09-14 15:28:32 +1000

    Fixed that init.rb was referencing wrong module name

--- init.rb
+++ init.rb
@@ -2,5 +2,5 @@
 require 'ardes/response_for_rc/actions'
 require 'ardes/response_for_rc/singleton_actions'
 
-Ardes::ResourcesController.actions = Ardes::ResponseForResourcesController::Actions
-Ardes::ResourcesController.singleton_actions = Ardes::ResponseForResourcesController::SingletonActions
+Ardes::ResourcesController.actions = Ardes::ResponseForRc::Actions
+Ardes::ResourcesController.singleton_actions = Ardes::ResponseForRc::SingletonActions
